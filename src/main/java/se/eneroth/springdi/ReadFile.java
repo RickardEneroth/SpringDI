55998c1efebf6dfbce2ec451e4a685a2161916a0
Rickard <rickard_eneroth@hotmail.com>
2015-02-17 20:22:52 +0100

    Readfile

---
+++ src/main/java/se/eneroth/springdi/ReadFile.java
@@ -0,0 +1,21 @@
+package se.eneroth.springdi;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by Rickard on 2015-02-17.
+ */
+public class ReadFile {
+
+    public static void main(String[] args) throws Exception{
+        System.out.println(readFile("resources/file.txt"));
+    }
+
+    static String readFile(String path) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, Charset.defaultCharset());
+    }
+}
